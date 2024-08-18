6c57c56626fa9a40230487d1581f69d6e0045e1d
Gerardo I. Ornelas <gerardo@ornelastechnologies.com>
2024-08-18 11:30:21 -0400

    update js sdk

--- src/crittora.ts
+++ src/crittora.ts
@@ -1,203 +1,183 @@
-import axios from "axios";
-import crypto from "crypto-js";
-
+import * as crypto from "crypto-js";
 import {
-  EncryptParams,
-  EncryptResponse,
-  Config,
-  DecryptParams,
-  DecryptResponse,
-  SignParams,
-  SignResponse,
-  VerifyParams,
-  VerifyResponse,
-  SignEncryptParams,
-  SignEncryptResponse,
-  DecryptVerifyParams,
-  DecryptVerifyResponse,
+  AuthTokens,
+  SignEncryptPayload,
+  DecryptVerifyPayload,
+  EncryptPayload,
+  DecryptPayload,
+  Headers,
 } from "./types";
-import {
-  COGNITO_API_URL,
-  POST_ENCRYPT_URL,
-  POST_DECRYPT_URL,
-  POST_SIGN_URL,
-  POST_VERIFY_URL,
-  POST_SIGN_ENCRYPT_URL,
-  POST_DECRYPT_VERIFY_URL,
-} from "./constants";
 
 class Crittora {
-  private config: Config;
-  private currentAccessToken: string | null = null;
-  private accessTokenExpiry: number | null = null;
+  private readonly clientId: string;
+  private readonly clientSecret: string;
+  private readonly userPoolId: string;
+  private readonly baseUrl: string =
+    "https://cognito-idp.us-east-1.amazonaws.com/";
 
-  constructor(config: Config) {
-    this.config = config;
-    this.currentAccessToken = config.currentAccessToken || null;
-    this.accessTokenExpiry = config.accessTokenExpiry || null;
+  constructor(clientId: string, clientSecret: string, userPoolId: string) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.userPoolId = userPoolId;
   }
 
-  private generateSecretHash(
-    username: string,
-    clientId: string,
-    clientSecret: string
-  ): string {
-    const message = username + clientId;
-    const hash = crypto.HmacSHA256(message, clientSecret);
+  private generateSecretHash(username: string): string {
+    const message = username + this.clientId;
+    const hash = crypto.HmacSHA256(message, this.clientSecret);
     return crypto.enc.Base64.stringify(hash);
   }
 
-  private async fetchAccessToken(): Promise<void> {
-    const {
-      credentialsUsername,
-      credentialsPassword,
-      cognitoPoolClientId,
-      clientId,
-      clientSecret,
-      fetchTokenOnEveryRequest,
-    } = this.config;
-
-    if (fetchTokenOnEveryRequest) {
-      this.currentAccessToken = null;
-      this.accessTokenExpiry = null;
+  public async authenticate(
+    username: string,
+    password: string
+  ): Promise<string> {
+    if (!username || !password) {
+      throw new Error("Username and password are required");
     }
 
-    const currentTime = new Date().getTime();
-    if (
-      this.currentAccessToken &&
-      this.accessTokenExpiry &&
-      currentTime < this.accessTokenExpiry
-    ) {
-      return;
-    }
-
-    const secretHash = this.generateSecretHash(
-      credentialsUsername,
-      clientId,
-      clientSecret
-    );
+    const secretHash = this.generateSecretHash(username);
 
     const body = {
       AuthParameters: {
-        USERNAME: credentialsUsername,
-        PASSWORD: credentialsPassword,
+        USERNAME: username,
+        PASSWORD: password,
         SECRET_HASH: secretHash,
       },
       AuthFlow: "USER_PASSWORD_AUTH",
-      ClientId: clientId,
+      ClientId: this.clientId,
     };
 
     try {
-      const response = await axios.post(COGNITO_API_URL, body, {
+      const response = await fetch(this.baseUrl, {
+        method: "POST",
         headers: {
           "Content-Type": "application/x-amz-json-1.1",
           "X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth",
         },
+        body: JSON.stringify(body),
       });
 
-      const responseData = response.data;
-      if (responseData.AuthenticationResult) {
-        this.currentAccessToken = responseData.AuthenticationResult.IdToken;
-        this.accessTokenExpiry =
-          new Date().getTime() +
-          responseData.AuthenticationResult.ExpiresIn * 1000;
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const responseJson = await response.json();
+
+      if (responseJson.AuthenticationResult?.IdToken) {
+        return responseJson.AuthenticationResult.IdToken;
       } else {
-        throw new Error("AuthenticationResult not found in the response");
+        throw new Error("Authentication failed: No IdToken received");
       }
     } catch (error) {
-      this.handleError(error, "Failed to fetch access token");
+      console.error("Authentication error:", error);
+      throw new Error("Authentication failed");
     }
   }
 
-  private async makeAuthenticatedRequest<T>(
+  private createHeaders(
+    idToken: string,
+    apiKey: string,
+    accessKey: string,
+    secretKey: string
+  ): Headers {
+    return {
+      Authorization: `Bearer ${idToken}`,
+      "x-api-key": apiKey,
+      "x-access-key": accessKey,
+      "x-secret-key": secretKey,
+      "Content-Type": "application/json",
+    };
+  }
+
+  private async makeApiCall(
     url: string,
-    params: any,
-    errorMessage: string
-  ): Promise<T> {
-    await this.fetchAccessToken();
+    headers: Headers,
+    payload: any
+  ): Promise<Response> {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(payload),
+    });
 
-    console.log("Request Params:", params);
-
-    try {
-      const response = await axios.post(url, params, {
-        headers: {
-          Authorization: `Bearer ${this.currentAccessToken}`,
-          api_key: this.config.api_key,
-          access_key: this.config.access_key,
-          secret_key: this.config.secret_key,
-          "Content-Type": "application/json",
-        },
-      });
-      console.log("Response Data:", response.data);
-      return response.data;
-    } catch (error) {
-      this.handleError(error, errorMessage);
+    if (!response.ok) {
+      throw new Error(
+        `API call failed: ${response.status} ${response.statusText}`
+      );
     }
-  }
 
-  private handleError(error: unknown, defaultMessage: string): never {
-    if (axios.isAxiosError(error)) {
-      console.error("Error Response:", error.response?.data || error.message);
-      throw new Error(`${defaultMessage}: ${error.message}`);
-    } else if (error instanceof Error) {
-      console.error("Error:", error.message);
-      throw new Error(`${defaultMessage}: ${error.message}`);
-    } else {
-      throw new Error(defaultMessage);
-    }
-  }
-
-  public async encrypt(params: EncryptParams): Promise<EncryptResponse> {
-    return this.makeAuthenticatedRequest<EncryptResponse>(
-      POST_ENCRYPT_URL,
-      params,
-      "Failed to encrypt data"
-    );
-  }
-
-  public async decrypt(params: DecryptParams): Promise<DecryptResponse> {
-    return this.makeAuthenticatedRequest<DecryptResponse>(
-      POST_DECRYPT_URL,
-      params,
-      "Failed to decrypt data"
-    );
-  }
-
-  public async sign(params: SignParams): Promise<SignResponse> {
-    return this.makeAuthenticatedRequest<SignResponse>(
-      POST_SIGN_URL,
-      params,
-      "Failed to sign data"
-    );
-  }
-
-  public async verify(params: VerifyParams): Promise<VerifyResponse> {
-    return this.makeAuthenticatedRequest<VerifyResponse>(
-      POST_VERIFY_URL,
-      params,
-      "Failed to verify data"
-    );
+    return response;
   }
 
   public async signEncrypt(
-    params: SignEncryptParams
-  ): Promise<SignEncryptResponse> {
-    return this.makeAuthenticatedRequest<SignEncryptResponse>(
-      POST_SIGN_ENCRYPT_URL,
-      params,
-      "Failed to sign / encrypt data"
-    );
+    idToken: string,
+    apiKey: string,
+    accessKey: string,
+    secretKey: string,
+    data: string,
+    permissions?: string[],
+    url: string = "SIGN_ENCRYPT_URL"
+  ): Promise<Response> {
+    const headers = this.createHeaders(idToken, apiKey, accessKey, secretKey);
+    const payload: SignEncryptPayload = {
+      data: data,
+      requested_actions: ["e", "s"],
+      permissions: permissions,
+    };
+    return this.makeApiCall(url, headers, payload);
   }
 
   public async decryptVerify(
-    params: DecryptVerifyParams
-  ): Promise<DecryptVerifyResponse> {
-    return this.makeAuthenticatedRequest<DecryptVerifyResponse>(
-      POST_DECRYPT_VERIFY_URL,
-      params,
-      "Failed to verify / decrypt data"
-    );
+    idToken: string,
+    apiKey: string,
+    accessKey: string,
+    secretKey: string,
+    encryptedData: string,
+    permissions?: string[],
+    url: string = "DECRYPT_VERIFY_URL"
+  ): Promise<Response> {
+    const headers = this.createHeaders(idToken, apiKey, accessKey, secretKey);
+    const payload: DecryptVerifyPayload = {
+      encrypted_data: encryptedData,
+      permissions: permissions,
+    };
+    return this.makeApiCall(url, headers, payload);
+  }
+
+  public async encrypt(
+    idToken: string,
+    apiKey: string,
+    accessKey: string,
+    secretKey: string,
+    data: string,
+    permissions?: string[],
+    url: string = "ENCRYPT_URL"
+  ): Promise<Response> {
+    const headers = this.createHeaders(idToken, apiKey, accessKey, secretKey);
+    const payload: EncryptPayload = {
+      data: data,
+      requested_actions: ["e"],
+      permissions: permissions,
+    };
+    return this.makeApiCall(url, headers, payload);
+  }
+
+  public async decrypt(
+    idToken: string,
+    apiKey: string,
+    accessKey: string,
+    secretKey: string,
+    encryptedData: string,
+    permissions?: string[],
+    url: string = "DECRYPT_URL"
+  ): Promise<Response> {
+    const headers = this.createHeaders(idToken, apiKey, accessKey, secretKey);
+    const payload: DecryptPayload = {
+      encrypted_data: encryptedData,
+      permissions: permissions,
+    };
+    return this.makeApiCall(url, headers, payload);
   }
 }
 
-export { Crittora, Config, EncryptParams, EncryptResponse };
+export default Crittora;
--- test/crittora.test.ts
+++ test/crittora.test.ts
@@ -1,127 +1,313 @@
-import { Crittora } from "../src/crittora";
+import dotenv from "dotenv";
+import Crittora from "../src/crittora";
 
-const config = {
-  credentialsUsername: "testuser5",
-  credentialsPassword: "jn]{`:s6&T6-qqHd",
-  cognitoPoolClientId: "a46804b8-20a1-708d-f5d0-13fe0ae1bfa7",
-  clientId: "5g9pp889dcru8php2p5ihvoosr",
-  clientSecret: "1ln5m524mjvi7ah9v0060498nhlhkdf06k5avthuu7dgqht7biqf",
-  api_key: "ma,{fKV!mLTVBEY)U#Bi(@Y-r;RJ.684*SF:dB!.H}R.c|>{[!y't]6bm{K=Cl}c",
-  secret_key:
-    "*q4@?D</qmrhfF9I_[_Rt_?(wV2CE:n'!beg<{#|A;2%:ETDgk}.ZseGPrz.td`A",
-  access_key: "6QNm1zR5.V:(X=Z~W(2SBO|7,Gw@jz4!",
-  fetchTokenOnEveryRequest: true,
-};
+// Load environment variables from .env file
+dotenv.config();
 
-const crittora = new Crittora(config);
+// Mock the global fetch function
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () =>
+      Promise.resolve({
+        AuthenticationResult: { IdToken: "mock-id-token", ExpiresIn: 3600 },
+      }),
+  })
+) as jest.Mock;
 
-const handleAction = async (action: Function, params: any) => {
-  try {
-    const response = await action(params);
-    const responseData = JSON.parse(response.body);
-    return responseData;
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(error.message);
-    } else {
-      throw new Error("An unknown error occurred");
-    }
-  }
-};
+describe("Crittora Class", () => {
+  const clientId = process.env.CLIENT_ID || "mock-client-id";
+  const clientSecret = process.env.CLIENT_SECRET || "mock-client-secret";
+  const userPoolId = process.env.USER_POOL_ID || "mock-user-pool-id";
 
-describe("Crittora SDK", () => {
-  jest.setTimeout(15000); // Extend timeout for async operations
+  let crittora: Crittora;
 
-  test("should encrypt data", async () => {
-    const params = {
-      data: "fake data",
-      requested_actions: ["e"],
-      permissions: [{ partner_id: "12345", permissions: ["d"] }],
-    };
+  beforeEach(() => {
+    crittora = new Crittora(clientId, clientSecret, userPoolId);
+    jest.clearAllMocks();
+  });
 
-    const response = await handleAction(
-      crittora.encrypt.bind(crittora),
-      params
+  const mockRequest = async (
+    methodName: keyof Crittora,
+    url: string,
+    payload: object,
+    idToken = process.env.ID_TOKEN || "mock-id-token"
+  ) => {
+    const apiKey = process.env.API_KEY || "mock-api-key";
+    const accessKey = process.env.ACCESS_KEY || "mock-access-key";
+    const secretKey = process.env.SECRET_KEY || "mock-secret-key";
+
+    await (crittora[methodName] as Function)(
+      idToken,
+      apiKey,
+      accessKey,
+      secretKey,
+      ...Object.values(payload)
     );
-    expect(response).toHaveProperty("encrypted_data");
-  });
 
-  test("should decrypt data", async () => {
-    const params = {
-      transactionId: "4003bacc-6107-4dd6-b7be-116fe8b590d2",
-      encryptedData: "44hb88caQ3EE7p3pOp3bIHgUGhlQkW70SFUekDiTpcY=",
-      requested_actions: ["d"],
-    };
-
-    const response = await handleAction(
-      crittora.decrypt.bind(crittora),
-      params
-    );
-    expect(response).toHaveProperty("decrypted_data");
-  });
-
-  test("should sign data", async () => {
-    const params = {
-      data: "fake data",
-      requested_actions: ["s"],
-      permissions: [{ partner_id: "12345", permissions: ["d"] }],
-    };
-
-    const response = await handleAction(crittora.sign.bind(crittora), params);
-    expect(response).toHaveProperty("signature");
-  });
-
-  test("should verify data", async () => {
-    const params = {
-      signedData: "fake data",
-      transactionId: "d6838cb6-89c4-459b-9919-68ab40f285c9",
-      signature:
-        "8e082a42335c71b64fe228de8842512c1881f7fabbb561b77de0ca97e5c273926099554d686806d9da42c48ae8ad631f914681f25c8595f1bb1264f5c7a18508",
-      requested_actions: ["v"],
-    };
-
-    const response = await handleAction(crittora.verify.bind(crittora), params);
-    expect(response).toHaveProperty("is_valid_signature");
-  });
-
-  test("should sign and encrypt data", async () => {
-    const params = {
-      data: "fake data for sign and encrypt",
-      requested_actions: ["e", "s"],
-      permissions: [{ partner_id: "12345", permissions: ["d"] }],
-    };
-
-    const response = await handleAction(
-      crittora.signEncrypt.bind(crittora),
-      params
-    );
-    expect(response).toHaveProperty("encryption");
-    expect(response.encryption).toHaveProperty("transactionid");
-    expect(response.encryption).toHaveProperty("encrypted_data");
-    expect(response).toHaveProperty("signature");
-    expect(response.signature).toHaveProperty("transactionid");
-    expect(response.signature).toHaveProperty("signature");
-  });
-  test("should decrypt and verify data", async () => {
-    const params = {
-      encryption: {
-        transactionId: "ad052f2c-f7f6-466f-a07d-1ac9dc04ac22",
-        encryptedData:
-          "kmdHx5MEt5Dv3OJsORASCPFalDTMr806Yn4HbuqcsQDVLNWG7ZpByMWwwh-_bATb",
+    expect(fetch).toHaveBeenCalledWith(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+        "x-api-key": apiKey,
+        "x-access-key": accessKey,
+        "x-secret-key": secretKey,
+        "Content-Type": "application/json",
       },
-      signing: {
-        transactionId: "a0c546b7-019f-44e5-bab9-f8430d073488",
-        signature:
-          "52c06ffaa83e562feb5d802a512955e67628800585d82fc7029ba454ff2eef4ff8d52fe3f5e7988363141a9d4295389aa723b2eba9ddde742da0ca056a333204",
-      },
-      requested_actions: ["v", "d"],
-    };
+      body: JSON.stringify(payload),
+    });
+  };
 
-    const response = await handleAction(
-      crittora.decryptVerify.bind(crittora),
-      params
-    );
-    expect(response).toHaveProperty("decrypted_data");
-    expect(response).toHaveProperty("is_valid_signature");
+  describe("authenticate", () => {
+    it("should return a valid IdToken when authentication is successful", async () => {
+      const username = process.env.USER_NAME || "mock-username";
+      const password = process.env.PASSWORD || "mock-password";
+
+      const idToken = await crittora.authenticate(username, password);
+
+      expect(idToken).toBe("mock-id-token");
+      expect(fetch).toHaveBeenCalledWith(
+        "https://cognito-idp.us-east-1.amazonaws.com/",
+        expect.any(Object)
+      );
+    });
+
+    it("should throw an error when authentication fails", async () => {
+      (global.fetch as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: false,
+          status: 400,
+          statusText: "Bad Request",
+        })
+      );
+
+      const username = process.env.USER_NAME || "mock-username";
+      const password = process.env.PASSWORD || "mock-password";
+
+      await expect(crittora.authenticate(username, password)).rejects.toThrow(
+        "Authentication failed"
+      );
+    });
+
+    it("should throw an error when username or password is missing", async () => {
+      await expect(crittora.authenticate("", "password")).rejects.toThrow(
+        "Username and password are required"
+      );
+      await expect(crittora.authenticate("username", "")).rejects.toThrow(
+        "Username and password are required"
+      );
+    });
+  });
+
+  describe("signEncrypt", () => {
+    it("should call fetch with the correct parameters", async () => {
+      const idToken = "mock-id-token";
+      const apiKey = "mock-api-key";
+      const accessKey = "mock-access-key";
+      const secretKey = "mock-secret-key";
+      const data = "mock-data";
+      const permissions = ["mock-permission"];
+      const url = "SIGN_ENCRYPT_URL";
+
+      (global.fetch as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+        })
+      );
+
+      await crittora.signEncrypt(
+        idToken,
+        apiKey,
+        accessKey,
+        secretKey,
+        data,
+        permissions,
+        url
+      );
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+          "x-api-key": apiKey,
+          "x-access-key": accessKey,
+          "x-secret-key": secretKey,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: data,
+          requested_actions: ["e", "s"],
+          permissions: permissions,
+        }),
+      });
+    });
+
+    it("should throw an error when the API call fails", async () => {
+      (global.fetch as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: false,
+          status: 500,
+          statusText: "Internal Server Error",
+        })
+      );
+
+      const idToken = "mock-id-token";
+      const apiKey = "mock-api-key";
+      const accessKey = "mock-access-key";
+      const secretKey = "mock-secret-key";
+      const data = "mock-data";
+
+      await expect(
+        crittora.signEncrypt(idToken, apiKey, accessKey, secretKey, data)
+      ).rejects.toThrow("API call failed: 500 Internal Server Error");
+    });
+  });
+
+  describe("decryptVerify", () => {
+    it("should call fetch with the correct parameters", async () => {
+      const encrypted_data =
+        process.env.ENCRYPTED_DATA || "mock-encrypted-data";
+      const permissions = ["mock-permission"];
+
+      (global.fetch as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+        })
+      );
+
+      await mockRequest("decryptVerify", "DECRYPT_VERIFY_URL", {
+        encrypted_data,
+        permissions,
+      });
+    });
+
+    it("should throw an error when the API call fails", async () => {
+      (global.fetch as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: false,
+          status: 400,
+          statusText: "Bad Request",
+        })
+      );
+
+      const idToken = "mock-id-token";
+      const apiKey = "mock-api-key";
+      const accessKey = "mock-access-key";
+      const secretKey = "mock-secret-key";
+      const encrypted_data = "mock-encrypted-data";
+
+      await expect(
+        crittora.decryptVerify(
+          idToken,
+          apiKey,
+          accessKey,
+          secretKey,
+          encrypted_data
+        )
+      ).rejects.toThrow("API call failed: 400 Bad Request");
+    });
+  });
+
+  describe("encrypt", () => {
+    it("should call fetch with the correct parameters", async () => {
+      const idToken = "mock-id-token";
+      const apiKey = "mock-api-key";
+      const accessKey = "mock-access-key";
+      const secretKey = "mock-secret-key";
+      const data = "mock-data";
+      const permissions = ["mock-permission"];
+      const url = "ENCRYPT_URL";
+
+      (global.fetch as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+        })
+      );
+
+      await crittora.encrypt(
+        idToken,
+        apiKey,
+        accessKey,
+        secretKey,
+        data,
+        permissions,
+        url
+      );
+
+      expect(fetch).toHaveBeenCalledWith(url, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+          "x-api-key": apiKey,
+          "x-access-key": accessKey,
+          "x-secret-key": secretKey,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: data,
+          requested_actions: ["e"],
+          permissions: permissions,
+        }),
+      });
+    });
+
+    it("should throw an error when the API call fails", async () => {
+      (global.fetch as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: false,
+          status: 403,
+          statusText: "Forbidden",
+        })
+      );
+
+      const idToken = "mock-id-token";
+      const apiKey = "mock-api-key";
+      const accessKey = "mock-access-key";
+      const secretKey = "mock-secret-key";
+      const data = "mock-data";
+
+      await expect(
+        crittora.encrypt(idToken, apiKey, accessKey, secretKey, data)
+      ).rejects.toThrow("API call failed: 403 Forbidden");
+    });
+  });
+
+  describe("decrypt", () => {
+    it("should call fetch with the correct parameters", async () => {
+      const encrypted_data =
+        process.env.ENCRYPTED_DATA || "mock-encrypted-data";
+      const permissions = ["mock-permission"];
+
+      (global.fetch as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: true,
+        })
+      );
+
+      await mockRequest("decrypt", "DECRYPT_URL", {
+        encrypted_data,
+        permissions,
+      });
+    });
+
+    it("should throw an error when the API call fails", async () => {
+      (global.fetch as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: false,
+          status: 401,
+          statusText: "Unauthorized",
+        })
+      );
+
+      const idToken = "mock-id-token";
+      const apiKey = "mock-api-key";
+      const accessKey = "mock-access-key";
+      const secretKey = "mock-secret-key";
+      const encrypted_data = "mock-encrypted-data";
+
+      await expect(
+        crittora.decrypt(idToken, apiKey, accessKey, secretKey, encrypted_data)
+      ).rejects.toThrow("API call failed: 401 Unauthorized");
+    });
   });
 });
