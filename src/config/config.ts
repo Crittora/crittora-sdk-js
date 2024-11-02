5db847f9d65b2ffdcb987bc0789fb2ac21cef486
Gerardo I. Ornelas <gerardo@ornelastechnologies.com>
2024-11-02 17:14:45 -0500

    remove dev urls

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,24 +9,16 @@ export interface CrittoraEnvironment {
 
 export class ConfigManager {
   private static instance: ConfigManager;
-  private readonly config: Record<string, CrittoraEnvironment>;
+  private readonly config: CrittoraEnvironment;
 
   private constructor() {
     dotenv.config();
 
     this.config = {
-      production: {
-        cognitoEndpoint: "https://cognito-idp.us-east-1.amazonaws.com/",
-        baseUrl: "https://api.crittoraapis.com",
-        userPoolId: "us-east-1_Tmljk4Uiw",
-        clientId: "5cvaao4qgphfp38g433vi5e82u",
-      },
-      development: {
-        cognitoEndpoint: "https://cognito-idp.us-east-1.amazonaws.com/",
-        baseUrl: "https://dev-api.crittoraapi.com",
-        userPoolId: "us-east-1_Zl27AI2Vr",
-        clientId: "5ok4074j0itrc27gbihn5s2bgn",
-      },
+      cognitoEndpoint: "https://cognito-idp.us-east-1.amazonaws.com/",
+      baseUrl: "https://api.crittoraapis.com",
+      userPoolId: "us-east-1_Tmljk4Uiw",
+      clientId: "5cvaao4qgphfp38g433vi5e82u",
     };
   }
 
@@ -37,15 +29,7 @@ export class ConfigManager {
     return ConfigManager.instance;
   }
 
-  getConfig(
-    environment: string = process.env.NODE_ENV || "development"
-  ): CrittoraEnvironment {
-    if (
-      process.env.npm_lifecycle_event?.includes("publish") ||
-      process.env.npm_lifecycle_event?.includes("build")
-    ) {
-      return this.config.production;
-    }
-    return this.config[environment] || this.config.development;
+  getConfig(): CrittoraEnvironment {
+    return this.config;
   }
 }
